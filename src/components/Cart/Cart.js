0fb9c52265d5a6a72bf111547e2eab6eba43d81d
agustinchavez <chavez13@illinois.edu>
2017-07-21 15:35:35 -0500

    redid cart calculate shipping

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -21,42 +21,24 @@ class Cart extends Component {
         }, 0);
         //take the total price of the items, the initial zero is what it starts with
 
-        this.price = totalPrice;
-        
+        var shipingPrice = this.props.cart.reduce((prev, item) => {
+            var quantity = 1;
+            var thisItemShippingPrice = item.shipping * quantity;
+            return thisItemShippingPrice + prev
+        }, 0);
+
         this.price = this.roundTwoDecimal(totalPrice);
+
+        this.shipping = this.roundTwoDecimal(shipingPrice);
+
         
     }
 
-    // totalPrice(){
-    //      var itemPrice = this.props.cart.reduce((prev, item) => item.price * item.quantity, 0);
-        
+    roundTwoDecimal(number) {
+        //make the price number two decimal places
+        return (Math.round(number * 100)/100);
+    }
 
-    //     this.price = this.getItemCount(itemPrice);
-    // }
-    
-    // getItemCount(number){
-    //     return number;
-    // }
-
-    // getItemCount() {
-    //     const cart = this.props.cart;
-    //     const itemKeys = Object.keys(cart);
-    //     return itemKeys.reduce((prev, key) => { 
-    //         return prev + cart[key];
-    //     }, 0);
-
-    // }
-
-    // getTotal(){
-    //     const cart = this.props.cart;
-    //     const itemKeys = Object.keys[cart];
-    //     const price = itemKeys.reduce((first, second) => {const item = fakeData.find(itm => itm.second === second);
-    //         const itemPrice = item.price * cart[second];
-    //         return first + itemPrice;
-    //     });
-
-    //     this.beforeTax = price;
-    // }
 
     render() {
         const cart = this.props.cart;
